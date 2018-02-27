473bbe5bc4707db5e301282be78bce13b6283baa
Barakat Abwe <brkatabweabwe@gmail.com>
2018-02-27 21:41:18 +0000

    Upload New File

---
+++ TestGraph.java
@@ -0,0 +1,17 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package Graph;
+
+/**
+ *
+ * @author theblackdevil
+ */
+public class TestGraph {
+    public static void main(String []args){
+        Graph graph=new Graph();
+        graph.printAdjMatrix();
+    }
+}
