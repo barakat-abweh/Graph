3a4d2d24caaffd2bdd94d0bcadf28df9ef986afc
Barakat Abwe <brkatabweabwe@gmail.com>
2018-03-02 10:27:26 +0000

    Update Node.java

--- Node.java
+++ Node.java
@@ -15,15 +15,18 @@ public class Node {
     private String name;
     boolean visited=false;
     private ArrayList<Node> neighbours;
+    private ArrayList<Integer> distances;
     public Node(String name){
         this.name=name;
         this.neighbours=new ArrayList<>();
+        this.distances=new ArrayList<>();
     }
     public ArrayList<Node> getNeighbours(){
         return this.neighbours;
     }
     public void addNeighbour(Node neighbour){
         this.neighbours.add(neighbour);
+        this.distances.add(1);
     }
     @Override
     public String toString(){
